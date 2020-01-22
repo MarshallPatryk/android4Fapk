f6140e484e1c4bf7ad5616b144ef6b59c5f35e8b
MarshallPatryk <marshallpatryk@gmail.com>
2020-01-22 21:10:24 +0100

    v1

--- app/src/main/java/com/gdgbangla/scannerproject/MainActivity.java
+++ app/src/main/java/com/gdgbangla/scannerproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.gdgbangla.scannerproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +14,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.zxing.Result;
 
@@ -23,15 +27,22 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
     public ZXingScannerView zXingScannerView;
     public TextView qrcode;
+    public TextView wynikSzukaniaEdit;
+    public EditText salonIDEdit;
+
+    public String salonOFFICIAL;
 
     private RequestQueue mQueue;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.select_salon);
 
+        //setContentView(R.layout.activity_main);
         qrcode  = (TextView) findViewById(R.id.wynikScan);
-        qrcode.setText("Tttt");
+        wynikSzukaniaEdit  = (TextView) findViewById(R.id.wynikSzukania);
+        salonIDEdit  = (EditText) findViewById(R.id.salonID);
+        //qrcode.setText("Tttt");
 
         mQueue = Volley.newRequestQueue(this);
     }
@@ -41,38 +52,50 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     //znajdz produkt po kodzie
     //znajdz model
     //wyswietl liste z produktami
-    private void jsonParse() {
 
-        String url = "https://api.myjson.com/bins/kp9wz";
+    String url = "173.249.20.230:3000/product/";
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("employees");
+    private void jsonParse(String kod) {
 
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject employee = jsonArray.getJSONObject(i);
+        String url = "http://173.249.20.230:3000/product/"+kod+"/?salon=gd12";
 
-                                String firstName = employee.getString("firstname");
-                                int age = employee.getInt("age");
-                                String mail = employee.getString("mail");
-
-                                //mTextViewResult.append(firstName + ", " + String.valueOf(age) + ", " + mail + "\n\n");
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
             @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
+            public void onResponse(String s) {
+                Log.d(s, "dane");
+                qrcode  = (TextView) findViewById(R.id.wynikScan);
+                qrcode.setText("test");
+                try {
+                    JSONObject obj = new JSONObject(s);
+                    qrcode.setText(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                qrcode.setText(s);
+
+
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                //Button btnaa  = (Button) findViewById(R.id.btnskan);
+                Log.d(String.valueOf(volleyError),"Some error occurred!!");
+                qrcode  = (TextView) findViewById(R.id.wynikScan);
+                qrcode.setText("Some error occurred!!");
             }
         });
 
-        mQueue.add(request);
+        RequestQueue rQueue = Volley.newRequestQueue(MainActivity.this);
+        rQueue.add(request);
+    }
+
+    public void selectSalon(View view){
+
+        salonOFFICIAL = String.valueOf(salonIDEdit.getText());
+        Toast.makeText(this, salonOFFICIAL, Toast.LENGTH_LONG).show();
+        //String[] dane = jsonParse(salonOFFICIAL);
+        //qrcode.setText(dane[0]);
+        setContentView(R.layout.activity_main);
     }
 
     public void scan(View view){
@@ -94,8 +117,7 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         Toast.makeText(getApplicationContext(),result.getText(),Toast.LENGTH_SHORT).show();
 
         setContentView(R.layout.activity_main); //stop camera
-        qrcode  = (TextView) findViewById(R.id.wynikScan);
-        qrcode.setText(result.getText());
+        jsonParse(result.getText());
 
         zXingScannerView.resumeCameraPreview(this);
 
